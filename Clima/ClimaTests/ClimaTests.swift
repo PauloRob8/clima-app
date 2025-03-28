068d40c6b929e8cf7ba903535cc417063b566c84
Paulo Roberto <paulo.rob159@gmail.com>
2025-03-28 09:27:01 -0300

    creating project

---
+++ Clima/ClimaTests/ClimaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaTests.swift
+//  ClimaTests
+//
+//  Created by Paulo Roberto on 28/03/25.
+//
+
+import Testing
+@testable import Clima
+
+struct ClimaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
